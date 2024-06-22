42ba68f35428d0fb443ad656ec34672895037a33
its-rajesh-smp <its.rajeshsmp@gmail.com>
2024-06-22 23:47:14 +0530

    ADD | REFACTOR  : Added Prisma and refactored the code & file structure.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import getEnv from 'utils/env/env.helpers';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const appPort = getEnv('PORT') || 3000;
+  await app.listen(appPort);
 }
 bootstrap();
