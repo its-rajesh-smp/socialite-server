056c0c684a49be0a24c8ee2ce5d14831ea2ba1f4
its-rajesh-smp <its.rajeshsmp@gmail.com>
2024-08-24 20:01:04 +0530

    FIX: Fixed the modals

---
+++ src/modules/prisma/migrations/20240824083256_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "PracticeSet" ALTER COLUMN "config" DROP NOT NULL;
---
+++ src/modules/prisma/migrations/20240824135844_/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "PracticeTask" ALTER COLUMN "status" SET DEFAULT 'LIVE';
+
+-- AlterTable
+ALTER TABLE "UserPracticeSet" ALTER COLUMN "status" SET DEFAULT 'LIVE';
