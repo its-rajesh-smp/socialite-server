3ebbba0459283832047a5b3356a45d244c7ef267
its-rajesh-smp <its.rajeshsmp@gmail.com>
2024-09-15 13:52:45 +0530

    REFACTOR: Deployment fix

--- src/modules/practice/taskTag/taskTag.resolver.ts
+++ src/modules/practice/taskTag/taskTag.resolver.ts
@@ -1,8 +1,8 @@
-import { User } from '@common/decorators/user.decorator';
 import IUser from '@common/types/user';
 import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { AuthGuard } from 'src/modules/auth/guards/auth.guard';
+import { User } from '../../../common/decorators/user.decorator';
+import { AuthGuard } from '../../auth/guards/auth.guard';
 import { TaskTagService } from './taskTag.service';
 
 /**
